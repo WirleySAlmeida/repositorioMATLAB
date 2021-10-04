b882cef1d27989bb57b91b61e9022e8f01756258
Wirley Almeida <wirley.s.almeida@gmail.com>
2021-10-04 19:04:51 -0300

    Aula 04/10

---
+++ Introduction to Programming with MATLAB/Week 5/check_out.m
@@ -0,0 +1,7 @@
+% Aula Input / Output
+
+function check_out(n, price)
+total = n*price;
+fprintf('%d items at %.2f each\nTotal = $%5.2f\n',n, price, total);
+end
+
---
+++ Introduction to Programming with MATLAB/Week 5/one_more.m
@@ -0,0 +1,6 @@
+% Aula Input / Output
+
+function a = one_more
+x = input('Gimme a number, buddy: ');
+a = x + 1;
+end

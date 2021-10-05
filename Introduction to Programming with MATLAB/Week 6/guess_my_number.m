45f4e077a6b75bebae31fc6a77987d4735ad0ab1
Wirley Almeida <wirley.s.almeida@gmail.com>
2021-10-05 16:49:38 -0300

    Começando semana 6

---
+++ Introduction to Programming with MATLAB/Week 6/guess_my_number.m
@@ -0,0 +1,10 @@
+% Aula Selection
+
+function guess_my_number(x)
+if x == 42
+    fprintf('Congrats! You guessed my number!\n');
+elseif x < 42
+    fprintf('Too small. Try again.\n');
+else
+    fprintf('Too big. Try again.');
+end
---
+++ Introduction to Programming with MATLAB/Week 6/guess_my_number_version_2.m
@@ -0,0 +1,8 @@
+% Aula Selection
+
+function guess_my_number(x)
+if x == 2
+    fprintf('Congrats! You guessed my number!\n');
+else
+    fprintf('Not right, but a good guess.\n');
+end

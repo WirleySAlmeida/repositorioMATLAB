d897ff3ac7cb4d9a98e8766202414fd8867d0f2b
Wirley Almeida <wirley.s.almeida@gmail.com>
2021-10-20 18:22:34 -0300

    Terminando semana 7

---
+++ Introduction to Programming with MATLAB/Week 7/noprealloc.m
@@ -0,0 +1,9 @@
+% Aula Preallocation
+
+function noprealloc
+N = 5000;
+for ii = 1:N
+    for jj = 1:N
+        A(ii,jj) = ii*jj;
+    end
+end
---
+++ Introduction to Programming with MATLAB/Week 7/prealloc.m
@@ -0,0 +1,10 @@
+% Aula Preallocation
+
+function prealloc
+N = 5000;
+A = zeros(N,N);
+for ii = 1:N
+    for jj = 1:N
+        A(ii,jj) = ii*jj;
+    end
+end
